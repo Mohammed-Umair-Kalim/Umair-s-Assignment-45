83fb6e0245ca444260e6777350311d29ad556428
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-03-22 08:04:11 -0700

    Typescript Assignment 45 completed

---
+++ 13_Your Own Array/main.ts
@@ -0,0 +1,8 @@
+// Q13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and
+// make a list that stores several examples. Use your list to print a series of statements about these items,
+// such as “I would like to own a Honda motorcycle.” ?
+
+// Answer 13:
+let myFavVehicles:string[]=["Chrysler ME412","Chevrolet Corvette Z06","Heavy Bike"]
+//For In Loop:
+for (let i in myFavVehicles){console.log(i,"-",myFavVehicles[i])}
